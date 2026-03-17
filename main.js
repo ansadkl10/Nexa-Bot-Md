57accc1fe472bafbc155826ea3a6072fdb91c5c1
arun•°Cumar <arunkumar773681@gmail.com>
2026-03-17 07:16:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { startNexa } from './Connection.js' 
+import { startNexa } from './connection.js' 
 import config from "./config.js"
 console.log(config.BOT_NAME)
 console.log(config.OWNER_NUMBER)
