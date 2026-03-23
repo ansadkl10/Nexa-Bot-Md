2686c9fd41e7ed8cd96b8bb072ecab97a8af8f02
arun•°Cumar <arunkumar773681@gmail.com>
2026-03-23 11:42:46 +0530

    Update loader.js
    Signed-off-by: arun•°Cumar  <arunkumar773681@gmail.com>

--- settings/loader.js
+++ settings/loader.js
@@ -1,42 +1,23 @@
-import path from "path";
-import { pathToFileURL } from "url";
-import fs from "fs";
+import stickerHandler from '../commands/sticker.js';
+import aliveHandler from '../commands/alive.js';
 
-export const executeCommand = async (commandName, sock, msg, args, extra) => {
+export default async (commandName, sock, msg, args, extra) => {
+    const { isOwner, isAdmin } = extra;
+
+    if (commandName === 'sticker' || commandName === 's') {
+        await stickerHandler(sock, msg, args);
+    } 
     
-    const safeCommandName = path.basename(commandName);
-    const commandPath = path.join(process.cwd(), "plugins", `${safeCommandName}.js`);
+    else if (commandName === 'alive') {
+        await aliveHandler(sock, msg);
+    }
+    
+     else if (commandName === 'ping') {
+        await pingHandler(sock, msg);
+    }
 
-    try {
+    else {
         
-        if (!fs.existsSync(commandPath)) {
-            return false;
-        }
-
-        const moduleUrl = `${pathToFileURL(commandPath).href}?update=${Date.now()}`;
-        const commandModule = await import(moduleUrl);
-
-        const handler = commandModule.default || commandModule.run || commandModule.execute;
-
-        if (typeof handler === "function") {
-            
-            await Promise.race([
-                handler(sock, msg, args, extra),
-                new Promise((_, reject) => 
-                    setTimeout(() => reject(new Error("Command Timeout")), 15000)
-                )
-            ]);
-            return true;
-        }
-
-        console.warn(`[Warning]: Command ${safeCommandName} has no valid export function.`);
-        return false;
-
-    } catch (err) {
-        // 6. Detailed Error Logging without crashing the main process
-        console.error(`[Command Error] -> "${commandName}":`, err.message);
-        
-        return false;
+        console.log(`Unknown command: ${commandName}`);
     }
 };
-
