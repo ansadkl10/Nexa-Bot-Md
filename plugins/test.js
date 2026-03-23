74e381065cc0fb485d5b613e50a9ba76eb418555
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-03-23 16:23:13 +0530

    Delete plugins/test.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

--- plugins/test.js
+++
@@ -1,4 +0,0 @@
-export default async (sock, msg, args, extra) => {
-    const chat = msg.key.remoteJid;
-    await sock.sendMessage(chat, { text: '✅ *BOT IS WORKING PERFECTLY!*\n\nTest passed 🎉' }, { quoted: msg });
-};
