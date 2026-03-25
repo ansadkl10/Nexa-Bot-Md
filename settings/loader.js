d8e9c869e2e6e2b8f9fd2e1de8f2611b03782cee
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-03-25 11:27:57 +0530

    Update loader.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

--- settings/loader.js
+++ settings/loader.js
@@ -1,6 +1,6 @@
 // © 2026 arun•°Cumar. All Rights Reserved.  
 import { fquoted } from './quoted.js'; 
-import ownerHandler from './plugins/owner.js';
+import ownerHandler from '../plugins/owner.js';
 import menuHandler from '../plugins/menu.js';  
 import aliveHandler from '../plugins/alive.js';  
 import pingHandler from '../plugins/ping.js';  
